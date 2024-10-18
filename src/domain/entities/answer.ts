1a63a142005ceadd2b5643857f203281ed74e21f
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-10-18 13:11:07 -0400

    feat add question and answer

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -13,6 +13,36 @@ export class Answer extends Entity<answerProps> {
     return this.props.content
   }
 
+  get questionId() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.content
+  }
+
+  get updatedAt() {
+    return this.props.createdAt
+  }
+
+  get excerpt() {
+    return this.content
+      .substring(0,120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+
+
   static create(props: Optional<answerProps, 'createdAt'>, id?: UniqueEntityID) {
     const answer = new Answer({
       ...props,
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../types/optional"
@@ -14,6 +15,60 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+  
+   get excerpt() {
+    return this.content
+      .substring(0,120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  } 
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
   static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
     const question = new Question({
       ...props,
